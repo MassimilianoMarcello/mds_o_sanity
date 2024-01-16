8fcb167e583ca89679f46403b09ec68da3c6d887
massimiliano <massimarcello@hotmail.it>
2024-01-17 00:07:27 +0100

    Global styles Styled components set

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Early from './earlychidhood/page'
-import Ear from './leadership/page';
+import GlobalStyles from '@/app/styled-global';
+
 
 export default function Home() {
   
   return (
     <main className={styles.main}>
+      <GlobalStyles/>
       <div>hola</div>
       <Early/>
-      <Ear/>
+    
     </main>
   )
 }
