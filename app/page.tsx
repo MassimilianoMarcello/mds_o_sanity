c83d118e3804ab65e28db17e78ad1c6ff9dafe5c
massimiliano <massimarcello@hotmail.it>
2024-01-17 01:17:57 +0100

    theme provider

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,18 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import Early from './earlychidhood/page'
 import GlobalStyles from '@/app/styled-global';
-
+import { ThemeProvider } from "styled-components";
+import theme from '@/app/Theme';
 
 export default function Home() {
   
   return (
     <main className={styles.main}>
+        <ThemeProvider theme={theme}>
       <GlobalStyles/>
       <div>hola</div>
       <Early/>
-    
+      </ThemeProvider>
     </main>
   )
 }
